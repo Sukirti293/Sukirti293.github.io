bb511e435817b366fd2a2c66f7ec3c6f00c8c9c7
Sukirti Dash <sdash@dons.usfca.edu>
2020-10-05 23:38:53 -0700

    first

---
+++ precache-manifest.e62bddc3b4d142004b82ca8e8fc2ab5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366ef77341fb81e2621e47b6ac94d153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a294624f0b09df6905e",
+    "url": "/static/css/main.0b5fc643.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a294624f0b09df6905e",
+    "url": "/static/js/main.19cf7eea.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "b8cba3b5a0bde2c19d9d280ac6b076c8",
+    "url": "/static/media/Operator-Book-Pro.b8cba3b5.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
